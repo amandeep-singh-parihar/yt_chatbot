b0ac852a6d6972e98c14c3c3407197f3d4515186
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2026-07-30 15:49:05 +0530

    FEAT : setting up text area

--- components/forms/UserForm.tsx
+++ components/forms/UserForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 import {useForm, SubmitHandler} from "react-hook-form"
+import { useState } from "react"
 
 type Props = {}
 
@@ -14,12 +15,37 @@ function UserForm({}: Props) {
     handleSubmit,
     formState:{errors}
   } = useForm<FormInput>()
-  const onSubmit: SubmitHandler<FormInput> = (data) => {
+
+  const [text, setText] = useState("");
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
     console.log(data);
     const video_id = data.video_url.split("v=")[1];
+    const query = data.query
     console.log(video_id)
+    try {
+        const response = await fetch("http://localhost:8000/generate", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            video_id: video_id,
+            query: query
+          })
+      });
+
+      const data = await response.json();
+      if(data) {
+        setText(data.response)
+      }
+      console.log(data)
+    } catch (error) {
+      
+    }
   }
   return (
+    <>
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className='flex items-center justify-center flex-col'>
         <input type="text" {...register("video_url", {required:true})} className='border-2'/>
@@ -31,6 +57,8 @@ function UserForm({}: Props) {
         <button type='submit'>Submit</button>
       </div>
     </form>
+    <textarea name="" id="" value={text} className="border-2"/>
+    </>
   )
 }
 
